d60657df4b08d9406474a1107c8b96300e2690f6
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-08-19 10:46:28 +0200

    Add basic subevent matching

--- patchback/event_handlers.py
+++ patchback/event_handlers.py
@@ -13,9 +13,35 @@ logger = logging.getLogger(__name__)
 @process_event_actions('pull_request', {'labeled'})
 @process_webhook_payload
 # pylint: disable=too-many-locals
-async def on_label_change(*, labels, number, **_kwargs):
+async def on_label_change(
+        *,
+        labels,  # list of PR labels
+        # https://docs.github.com/en/rest/reference/pulls#get-a-pull-request:
+        # phantom merge commit sha if it's open or closed and not merged;
+        # real merge commit sha, if it's merged.
+        merge_commit_sha,
+        merged,  # flag whether it's merged or not
+        number,  # PR number
+        **_kwargs,  # unimportant event details
+) -> None:
     """React to GitHub App pull request / issue label webhook event."""
+    if not merged:
+        logger.info('PR#%s is not merged, ignoring...', number)
+        return
+
+    backport_label_prefix = 'backport-'
+    backport_label_len = len(backport_label_prefix)
+    target_branches = [
+        label[backport_label_len:] for label in labels
+        if label.startswith(backport_label_prefix)
+    ]
+
+    if not target_branches:
+        logger.info('PR#%s does not have backport labels, ignoring...', number)
+        return
+
     gh_api = RUNTIME_CONTEXT.app_installation_client
 
     logger.info('PR#%s labels got updated to: %r', number, labels)
+    logger.info('PR#%s merge commit: %s', merge_commit_sha)
     logger.info('gh_api=%s', gh_api)
